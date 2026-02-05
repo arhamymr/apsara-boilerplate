b9ab26fd9c169d8a132ba1b9c3b2c7bcfcacd1b0
arhamymr <arhamymr@gmail.com>
2026-02-05 21:06:26 +0700

    update

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -2,14 +2,43 @@ import { betterAuth } from "better-auth"
 import { openAPI } from "better-auth/plugins";
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/db/database';
+import { sendEmail } from '@/lib/email';
+
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
     }),
-    emailAndPassword: {    
-        enabled: true
+    
+    emailAndPassword: {
+        enabled: true,
+        requireEmailVerification: false,
+
+        sendResetPassword: async ({ user, url, token }, request) => {
+            void sendEmail({
+                to: user.email,
+                subject: "Reset your password",
+                text: `Click the link to reset your password: ${url}`,
+            });
+        },
+        onPasswordReset: async ({ user }, request) => {
+            // your logic here
+            console.log(`Password for user ${user.email} has been reset.`);
+        },
     },
+    
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            void sendEmail({
+                to: user.email,
+                subject: "Verify your email address",
+                text: `Click the link to verify your email: ${url}`,
+            });
+        },
+    },
+
+
+    // plugins
     plugins: [
         openAPI(),
     ]
